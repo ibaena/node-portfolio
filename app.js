ebabacb90b398ff53b5e8085d96987526ccab763
Ivan <ivan.baena04@gmail.com>
2016-02-11 07:47:15 -0500

    almost finished POST request for contact form log results to log.txt, next need to clear input on submit

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
 var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use('/public', express.static(__dirname + "/public"));
+
+function myLoggingMiddleware(req, res, next){
+  var url = req.url;
+  var method = req.method;
+
+  console.log('%s request at %s', method, url);
+  next();
+}
+
+// This is the bodyParser middleware
+ app.use(bodyParser.urlencoded({ extended: false }));
+
+ app.use(myLoggingMiddleware);
 
 app.get('/', function(req, res){
   res.render('index', {
@@ -39,8 +54,15 @@ app.get('/about', function(req, res){
 
 app.get('/blog', function(req, res){
   res.render('blogger', {
-       title : 'Dishing and Swishing',
+       title : 'ThisThat',
        layout : 'blog'
+});
+});
+
+app.post('/contact', function(req, res){
+  var form = 'Name: '+req.body.username+'\nEmail: '+ req.body.email+'\nMessage: '+ req.body.summary+'\n\n';
+  fs.appendFile('log.txt',form , function (err) {
+
 });
 });
 
